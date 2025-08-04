7fd910a250153c6db07d63a17f28aa5d9a24fac0
patrickkuei <patrickkuei@users.noreply.github.com>
2025-08-04 12:40:54 +0000

    deploy: 2f44b8a41ab3c76eb59c1cd213857d14f93872fe

---
+++ storybook/assets/index-BBwP5Ey1.js
@@ -0,0 +1,1 @@
+const E=["Home","Interactive","Elements","Feedback"],e={SEGMENT_COUNT:20,SEGMENT_ANGLE:18,GAP_ANGLE:4,INNER_RADIUS:18.5,OUTER_RADIUS:23.5,OUTER_TICK_RADIUS:30};export{e as P,E as T};
