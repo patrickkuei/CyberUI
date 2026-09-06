82b5c91c9cc031768dd0e19c4f494cda039cc42f
patrickkuei <patrickkuei@users.noreply.github.com>
2026-09-06 04:04:52 +0000

    deploy: 2cb5135dd95c3fa4e7e86caae515d1602331ef3e

---
+++ storybook/assets/index-CHO2xkCo.js
@@ -0,0 +1,1 @@
+const s=["Home","Interactive","Elements","Feedback","Console"],e={SEGMENT_COUNT:20,SEGMENT_ANGLE:18,GAP_ANGLE:4,INNER_RADIUS:18.5,OUTER_RADIUS:23.5,OUTER_TICK_RADIUS:30},t=["Arasaka Corporation","Militech Industries","Kang Tao Systems","NetWatch Division"];export{t as C,s as D,e as P};
